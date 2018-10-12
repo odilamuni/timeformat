e0a32c31bc5518f9a1915969e35c6367933809ae
odilamuni <odilamuni@gmail.com>
2018-10-12 11:47:15 -0400

    Updates

---
+++ precache-manifest.98674fc15409fb3f353988e2af0ff686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "823cad86157409ab580cc233845b7f47",
+    "url": "/timeformat/static/media/Icons-flecha.823cad86.png"
+  },
+  {
+    "revision": "81549b39780139916876",
+    "url": "/timeformat/static/js/runtime~main.81549b39.js"
+  },
+  {
+    "revision": "a9468940e570a56c16e7",
+    "url": "/timeformat/static/js/main.a9468940.chunk.js"
+  },
+  {
+    "revision": "e220c534aa951e58aecb",
+    "url": "/timeformat/static/js/1.e220c534.chunk.js"
+  },
+  {
+    "revision": "a9468940e570a56c16e7",
+    "url": "/timeformat/static/css/main.42aeaee0.chunk.css"
+  },
+  {
+    "revision": "473f1f6be23faf35c74d3496cb67e3e0",
+    "url": "/timeformat/index.html"
+  }
+];
